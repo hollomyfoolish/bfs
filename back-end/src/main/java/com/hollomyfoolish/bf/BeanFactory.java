ec83d2c1840dcc579d0140a84acfa9df3aa4c4ac
Allen <allen.gong@sap.com>
2018-09-17 07:16:58 +0800

    add spring jpa data

--- back-end/src/main/java/com/hollomyfoolish/bf/BeanFactory.java
+++ back-end/src/main/java/com/hollomyfoolish/bf/BeanFactory.java
@@ -1,7 +1,19 @@
 package com.hollomyfoolish.bf;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.hollomyfoolish.repo.RepoBeanCfg;
 import com.hollomyfoolish.service.ServiceBeanCfg;
@@ -11,6 +23,33 @@ import com.hollomyfoolish.service.ServiceBeanCfg;
 	RepoBeanCfg.class,
 	ServiceBeanCfg.class
 })
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class BeanFactory {
+
+	@Bean
+	public DataSource dataSource() {
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		return builder.setType(EmbeddedDatabaseType.HSQL).build();
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setGenerateDdl(true);
+
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setJpaVendorAdapter(vendorAdapter);
+		factory.setPackagesToScan("com.acme.domain");
+		factory.setDataSource(dataSource());
+		return factory;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager txManager = new JpaTransactionManager();
+		txManager.setEntityManagerFactory(entityManagerFactory);
+		return txManager;
+	}
 
 }
